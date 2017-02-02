eb68037e4bc10eeb76860a71ee4491af9b2cb408
Thomas D <Moneplokii@gmail.com>
2017-02-02 18:29:50 +0100

    Update main program.py

--- main program.py
+++ main program.py
@@ -1,31 +1,41 @@
-"""le programe suivant permet d'ouvrir une fenetre avec une toile, """
-
-#Importer les modules
 from tkinter import *
-from time import *
-from math import *
-from random import *
 
-def click(event):
-    coord_du_click_en_x=event.x
-    coord_du_click_en_y=event.y
-    toile.create_oval(coord_du_click_en_x-10,coord_du_click_en_y-10,coord_du_click_en_x+10,coord_du_click_en_y+10,fill='green')
-
-def trait_sous_click(event):
-    coord_du_click_en_x=event.x
-    coord_du_click_en_y=event.y
-    toile.create_oval(coord_du_click_en_x-1,coord_du_click_en_y-1,coord_du_click_en_x+1,coord_du_click_en_y+1,fill='blue',width=0) 
-
-def trait_continu(event):
-    coord_du_click_en_x=event.x
-    coord_du_click_en_y=event.y
-    toile.create_oval(coord_du_click_en_x,coord_du_click_en_y,coord_du_click_en_x,coord_du_click_en_y,fill='red',width=0)
-    
+""" def defile():
+    for ca in range(-50,50,10):
+        for cb in range(-50,50,10):
+            cc=complex(ca/100,cb/100)
+            fenetre.after(1,start(cc))
+            toile.update_idletasks"""
+def start(c):
+    toile.delete(ALL)
+    # if c != complex(0,0):
+    c=complex(ac.get(),ab.get())
+    print(c)
+    for rea in range(-200,200,3):
+        for ima in range(-200,200,3):
+            z=complex(rea/100,ima/100)
+            julia(z,c)
+            
+def julia(z,c):
+    Un = z
+    for loop in range(10):
+        Un = Un**2+c
+        if abs(Un)>3:break
+    if abs(Un)<1:
+        z=z*100+complex(500,500)
+        toile.create_oval(int(z.real)-2,int(z.imag)-2,int(z.real)+2,int(z.imag)+2,fill='black')
+        
 fenetre = Tk()
-fenetre.title("je suis un titre")
-toile = Canvas(fenetre, width=500, height=500, background='light grey')
-toile.bind("<Button-1>",click)
-toile.bind("<B1-Motion>",trait_sous_click)
-toile.bind("<Motion>",trait_continu)
+fenetre.title("TIPE")
+toile = Canvas(fenetre, width=1000, height=1000, background='white')
+ac=DoubleVar()
+ab=DoubleVar()
+c=complex(0,0)
+Label(fenetre,text='parti réel de C').pack()
+scaleac = Scale(fenetre,from_=-1, to=1,resolution=0.01,bg="indian red",orient=HORIZONTAL,width=10,variable=ac).pack()
+Label(fenetre,text='parti imaginaire de C').pack()
+scaleab = Scale(fenetre,from_=-1, to=1,resolution=0.01,bg="indian red",orient=HORIZONTAL,width=10,variable=ab).pack()
+boutstart=Button(fenetre,text="Commencer",command=lambda: start(c)).pack(side=TOP)
+#defiler=Button(fenetre,text="Commencer le défillement",command=lambda: defile()).pack(side=TOP)
 toile.pack()
 fenetre.mainloop()
