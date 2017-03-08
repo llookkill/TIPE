6c5e55d1d77c2a54dfde11f50043ffef8ddb5204
Thomas D <Moneplokii@gmail.com>
2017-03-08 16:28:17 +0100

    Create matrices.py

---
+++ matrices.py
@@ -0,0 +1,32 @@
+from random import *
+#Exercice n°1
+def afficheMat(mat):
+    nbl=len(mat)
+    nbc=len(mat[0])
+    for i in range(nbl):
+        print("(",end="")
+        for j in range(nbc):
+            print(" {:4} ".format(mat[i][j]),end="")
+        print(")")
+        
+#Exercice n°2
+def initMatVide(nbl,nbc):
+    m=[None]*nbl
+    for i in range (nbl):
+        m[i]=[None]*nbc
+    return m
+
+#Exercice n°3
+def initMatVal(nbl,nbc,val):
+    m=[val]*nbl
+    for i in range (nbl):
+        m[i]=[val]*nbc
+    return m
+
+#Exercice n°4
+def initMatAlea(nbl,nbc):
+    mat=initMatVide(nbl,nbc)
+    for i in range(nbl):
+        for j in range(nbc):
+            mat[i][j]=randint(0,20)
+    return mat
