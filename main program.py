10a8564b61bc991dbcc634038796afad574eceb4
Thomas D <Moneplokii@gmail.com>
2017-03-16 15:10:15 +0100

    Update main program.py

--- main program.py
+++ main program.py
@@ -1,5 +1,6 @@
 from tkinter import *
 
+
 def defile():
     cc=complex(0,0)
     for ca in range(int(ac.get()*100),int(ac.get()*100)+50,1):
@@ -13,6 +14,8 @@ def defile():
                     ):
         cc=complex(ca/100,ab.get())
         start(cc)
+
+
 def start(c):
     toile.delete(ALL)
     if c == complex(110,110): c=complex(ac.get(),ab.get())
@@ -24,24 +27,23 @@ def start(c):
         for ima in range(-300,300,1):
             z=complex(rea/150,ima/150)
             julia(z,c)
-            
+
 def julia(z,c):
-    nu=2
     Un = z
-    for loop in range(9):
+    for loop in range(6):
         Un = Un**2+c
-        if abs(Un)>256:break
+        if abs(Un)>8:break
         if abs(Un)<0.01:break
     z=z*149
     z=complex(z.real,-z.imag)+complex(500,300)
-    for dist in range (1,156):
-        if abs(Un)<dist*nu:
-            coul = '#%02x%02x%02x' % (int(dist)/nu+100,int(dist)/nu,int(dist*1+00)/nu)
-            if dist ==1:
-                coul="#0054A8"
+    for dist in range (100,801):
+        if abs(Un)<dist/100:
+            if dist > 100:coul = '#%02x%02x%02x' % (int(dist/801*256),int(dist/801*256),int(dist/801*256))
+            if dist <=100:coul = "#0054A8"
             toile.create_rectangle(round((z.real),0),round(int(z.imag)),round(int(z.real),0),round(int(z.imag)),fill=coul,width=0)
             toile.update()
             break
+
 
 fenetre = Tk()
 fenetre.title("TIPE")
