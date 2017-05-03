5b4ee78e62cf4e2b0004f8af4cd4f4a82b1d01af
Thomas D <Moneplokii@gmail.com>
2017-05-03 17:22:49 +0200

    Update OATP18String.py

--- OATP18String.py
+++ OATP18String.py
@@ -11,8 +11,14 @@ def affichechaîne2(chaine):
     for i in range(n-1):
         print(chaine[i],end=" ")
     print(chaine[n-1],end="")
-
+    
+def affichechaîne3(chaine):
+    for caract in chaine:
+        print(caract,end=" ")
+        
 lo="je suis un test"
 affichechaîne(lo)
 print("")
 affichechaîne2(lo)
+print("")
+affichechaîne3(lo)
