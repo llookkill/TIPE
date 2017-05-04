2e706ac37f6e914ecc2f31760826bbcec77040fd
Thomas D <Moneplokii@gmail.com>
2017-05-04 17:04:48 +0200

    Update main program.py

--- main program.py
+++ main program.py
@@ -1,49 +1,41 @@
 from tkinter import *
 
 
-def defile():
-    cc=complex(0,0)
-    for ca in range(int(ac.get()*100),int(ac.get()*100)+50,1):
-        cc=complex(ca/100,ab.get())
-
-        start(cc)
-    for ca in range(int(ac.get()*100+50),int(ac.get()*100)-50,-1):
-        cc=complex(ca/100,ab.get())
-        start(cc)
-    for ca in range(int(ac.get()*100-50),int(ac.get()*100)+5,+1
-                    ):
-        cc=complex(ca/100,ab.get())
-        start(cc)
-
-
 def start(c):
+    global pre
     toile.delete(ALL)
+    toile.create_rectangle(400,3,600,6,width=1)
     if c == complex(110,110): c=complex(ac.get(),ab.get())
     print(c)
     toile.create_text(950,50,text="C = {} + {}i".format(c.real,c.imag))
     toile.update()
-
-    for rea in range(-300,300,1):
-        for ima in range(-300,300,1):
+    pre=0
+    for rea in range(-300,300,pre+1):
+        for ima in range(-300,300,pre+1):
             z=complex(rea/150,ima/150)
-            julia(z,c)
+            nb=julia(z,c)
+            trace(z,nb)
+            
+def colorisation(nombre):
+    coul=(min(54+nombre*20,254),0,min(abs(254-nombre*20),254))
+    coul = str('#%02x%02x%02x' % coul)
+    return coul
+def trace(z,nb):
+    z=z*149 # homothétie 
+    z=complex(z.real,-z.imag)+complex(500,300) # complex(500,300) pour centrer la fractale
+    if nb == -1 : couleur= "Black"
+    else: couleur=colorisation(nb)
+    toile.create_rectangle(round((z.real)+pre,0),round(z.imag + pre , 0),round(int(z.real)-pre,0),round(int(z.imag))-pre,fill=couleur,width=0)
+
 
 def julia(z,c):
     Un = z
-    for loop in range(6):
+    for loop in range(100):
         Un = Un**2+c
-        if abs(Un)>8:break
-        if abs(Un)<0.01:break
-    z=z*149
-    z=complex(z.real,-z.imag)+complex(500,300)
-    for dist in range (100,801):
-        if abs(Un)<dist/100:
-            if dist > 100:coul = '#%02x%02x%02x' % (int(dist/801*256),int(dist/801*256),int(dist/801*256))
-            if dist <=100:coul = "#0054A8"
-            toile.create_rectangle(round((z.real),0),round(int(z.imag)),round(int(z.real),0),round(int(z.imag)),fill=coul,width=0)
-            toile.update()
+        if abs(Un) > 2 :
+            return loop
             break
-
+    if abs(Un) <= 2 : return -1
 
 fenetre = Tk()
 fenetre.title("TIPE")
@@ -51,13 +43,13 @@ toile = Canvas(fenetre, width=1000, height=600, background='white')
 ac=DoubleVar()
 ab=DoubleVar()
 c=complex(110,110)
-Label(fenetre,text='partie réelle de C').pack()
-scaleac = Scale(fenetre,from_=-2, to=2,resolution=0.01,bg="indian red",orient=HORIZONTAL,width=10,variable=ac).pack()
-Label(fenetre,text='partie imaginaire de C').pack()
-scaleab = Scale(fenetre,from_=-2, to=2,resolution=0.01,bg="indian red",orient=HORIZONTAL,width=10,variable=ab).pack()
+Label(fenetre,text='parti réel de C').pack()
+scaleac = Scale(fenetre,from_=-2, to=2,resolution=0.01,bg="brown",orient=HORIZONTAL,width=10,variable=ac).pack()
+Label(fenetre,text='parti imaginaire de C').pack()
+scaleab = Scale(fenetre,from_=-2, to=2,resolution=0.01,bg="brown",orient=HORIZONTAL,width=10,variable=ab).pack()
 boutstart=Button(fenetre,text="Commencer",command=lambda: start(c)).pack(side=TOP)
-defiler=Button(fenetre,text="Commencer le défilement",command=lambda: defile()).pack(side=TOP)
-bouton = Checkbutton(fenetre, text="Update continue ?")
+#defiler=Button(fenetre,text="Commencer le défillement",command=lambda: defile()).pack(side=TOP)
+bouton = Checkbutton(fenetre, text="Update continu ?")
 bouton.pack()
 toile.pack()
 fenetre.mainloop()
